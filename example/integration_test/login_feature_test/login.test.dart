d8db070c53db6b056a9826f29609b0a0aa3af429
ffhasnen <hasnen712@live.com>
2025-10-28 20:46:48 +0530

    fix

--- example/integration_test/login_feature_test/login.test.dart
+++ example/integration_test/login_feature_test/login.test.dart
@@ -2,6 +2,7 @@ import 'package:flutter_test/flutter_test.dart' hide group, testWidgets, expect;
 import 'package:flutter_testmate/flutter_testmate.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:example/main.dart' as app;
+import '../utils/key.dart';
 
 runTest() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
